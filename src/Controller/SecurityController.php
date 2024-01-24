57e2dfa33014ce50929480056e060a4acd1ce794
RaphaelAZ <azevedo.raphael2017@gmail.com>
2024-01-24 11:00:12 +0100

    (+) Registration done

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,13 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Form\UserLoginFormType;
 use App\Form\UserRegisterFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class SecurityController extends AbstractController
 {
@@ -23,15 +28,40 @@ class SecurityController extends AbstractController
     }
 
     #[Route('/inscription', name: 'inscription')]
-    public function register(Request $request): Response
+    public function register(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $userPasswordHasher): Response
     {
         $form = $this->createForm(UserRegisterFormType::class);
 
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            if(!$entityManager->getRepository(User::class)->findOneBy(['username' => $form->get('username')->getData()])) {
+            $user = new User();
+            $user->setUsername($form->get('username')->getData());
+            $user->setEmail($form->get('email')->getData());
+            $user->setPhone($form->get('phone')->getData());
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('password')->getData()
+                    )
+                );
+            $user->setRoles(['ROLE_USER']);
 
-            return $this->redirectToRoute('accueil');
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->render('security/register.html.twig', [
+                'form' => $form->createView(),
+                'success' => "Votre compte a bien été créé !"
+            ]);
+            }
+            else {
+                return $this->render('security/register.html.twig', [
+                    'form' => $form->createView(),
+                    'error' => "Le nom d'utilisateur renseigné existe deja !"
+                ]);
+            }
         }
 
         return $this->render('security/register.html.twig', [
--- src/Form/UserRegisterFormType.php
+++ src/Form/UserRegisterFormType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +15,26 @@ class UserRegisterFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
-            ->add('password')
-            ->add('email')
-            ->add('phone')
+            ->add('username', TextType::class, [
+                'label' => 'Nom d\'utilisateur',
+                'attr' => ['class' => 'form-control'],
+                'required' => true,
+            ])
+            ->add('password', PasswordType::class, [
+                'attr' => ['class' => 'form-control'],
+                'invalid_message' => 'Les champs de mot de passe doivent correspondre.',
+                'required' => true,
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'E-mail',
+                'attr' => ['class' => 'form-control'],
+                'required' => true,
+            ])
+            ->add('phone', TextType::class, [
+                'label' => 'N° Téléphone',
+                'attr' => ['class' => 'form-control'],
+                'required' => true,
+            ])
         ;
     }
 

bb5f93416062d6b856f4b48bd56aeb048a6fe662
RaphaelAZ <azevedo.raphael2017@gmail.com>
2024-02-01 16:21:48 +0100

    (+) final database

--- database/agence_immo.sql
+++ database/agence_immo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : ven. 26 jan. 2024 à 14:40
+-- Généré le : jeu. 01 fév. 2024 à 15:21
 -- Version du serveur : 8.0.31
 -- Version de PHP : 8.0.26
 
@@ -39,7 +39,7 @@ CREATE TABLE IF NOT EXISTS `announce` (
   `image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `creation` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Déchargement des données de la table `announce`
@@ -49,7 +49,7 @@ INSERT INTO `announce` (`id`, `title`, `price`, `surface`, `type`, `location`, `
 (2, 'Appartement spacieux', 159000, 80, 'Appartement', 'Lille', 'Joli petit appartement avec beaucoup de lumière, coin tranquille bien isolé, avec voisinage très aimable.', '7085f912acf6228797b667fa9f7344f9.jpg', '2024-01-26 10:13:45'),
 (3, 'Maison Familiale', 180000, 140, 'Maison', 'Avignon', 'Jolie maison familiale de 1980, refaite à neuf récemment hormis la devanture de la maison.', 'ddf5e0fb2667a2f4cfa139652c8e43ab.jpg', '2024-01-26 10:13:54'),
 (4, 'Studio 4 personnes', 96000, 40, 'Maison', 'Lille, Villeneuve d\'Ascq', 'Studio bien situé, proche des transports bien isolé et climatisé', '61047cdacdfb46f40b112aeddb00f5e8.jpg', '2024-01-26 10:14:03'),
-(7, 'Maison villa', 317000, 220, 'Maison', 'Nice, Bord de mer', 'Jolie Villa accès plage depuis la villa avec un grand terrain à l\'ombre le matin', '9391b820f39a47a99d480db752701757.jpg', '2024-01-26 10:14:18');
+(14, 'Jolie Villa', 333333, 230, 'Maison', 'Nice, Bord de mer', 'Jolie Villa accès plage', '07d55099c04242e46d7e405ef105495e.jpg', '2024-02-01 09:21:42');
 
 -- --------------------------------------------------------
 
@@ -78,7 +78,27 @@ INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_
 ('DoctrineMigrations\\Version20240124090152', '2024-01-24 09:02:02', 97),
 ('DoctrineMigrations\\Version20240125110918', '2024-01-25 11:10:36', 96),
 ('DoctrineMigrations\\Version20240126074156', '2024-01-26 07:42:38', 72),
-('DoctrineMigrations\\Version20240126105846', '2024-01-26 10:59:28', 337);
+('DoctrineMigrations\\Version20240126105846', '2024-01-26 10:59:28', 337),
+('DoctrineMigrations\\Version20240201142210', '2024-02-01 14:22:25', 451),
+('DoctrineMigrations\\Version20240201142527', '2024-02-01 14:25:33', 30),
+('DoctrineMigrations\\Version20240201150054', '2024-02-01 15:01:02', 29);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `favorite`
+--
+
+DROP TABLE IF EXISTS `favorite`;
+CREATE TABLE IF NOT EXISTS `favorite` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `announce_id` int DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  `date_favorite` date NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_68C58ED96F5DA3DE` (`announce_id`),
+  KEY `IDX_68C58ED9A76ED395` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -92,10 +112,21 @@ CREATE TABLE IF NOT EXISTS `pending_contact` (
   `announce_id` int DEFAULT NULL,
   `user_id` int DEFAULT NULL,
   `date_contact` date NOT NULL,
+  `phone` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pending` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `UNIQ_870D11A96F5DA3DE` (`announce_id`),
-  UNIQUE KEY `UNIQ_870D11A9A76ED395` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  KEY `IDX_870D11A96F5DA3DE` (`announce_id`),
+  KEY `IDX_870D11A9A76ED395` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `pending_contact`
+--
+
+INSERT INTO `pending_contact` (`id`, `announce_id`, `user_id`, `date_contact`, `phone`, `email`, `name`, `pending`) VALUES
+(1, 4, 1, '2024-02-01', '0659781248', 'michel@gmail.fr', 'Michel Dupont', 1);
 
 -- --------------------------------------------------------
 
@@ -126,6 +157,13 @@ INSERT INTO `user` (`id`, `username`, `roles`, `password`, `email`, `phone`) VAL
 --
 -- Contraintes pour les tables déchargées
 --
+
+--
+-- Contraintes pour la table `favorite`
+--
+ALTER TABLE `favorite`
+  ADD CONSTRAINT `FK_68C58ED96F5DA3DE` FOREIGN KEY (`announce_id`) REFERENCES `announce` (`id`),
+  ADD CONSTRAINT `FK_68C58ED9A76ED395` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
 
 --
 -- Contraintes pour la table `pending_contact`
